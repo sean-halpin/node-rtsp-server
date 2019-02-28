c55bcbcd001e4d486901fb26ccce792b6e4021df
Sean <pulpfictionost@gmail.com>
2019-02-28 19:52:07 +0000

    Add Packages & Calculate Sdp Length

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var path=require('path');
+var path = require('path');
+var getLength = require("utf8-byte-length")
 var net = require('net');
 var shell = require('shelljs');
 var HashMap = require('hashmap');
@@ -36,7 +37,7 @@ function handleConnection(conn) {
         var response = "NO RESPONSE SET";
         var messageType = lines[0].split(" ")[0];
         switch (messageType) {
-                case "RTSP/1.0":
+            case "RTSP/1.0":
                 response = RTSP_501;
                 response += "CSeq: " + headers.get("CSeq") + "\r\n";
                 response += "Server: " + SERVER_NAME + "\r\n";
@@ -57,32 +58,16 @@ function handleConnection(conn) {
                 // Accept: application / sdp
                 // CSeq: 2
                 // User - Agent: Lavf57.83.100
+                var sdp = generateSdp();
+                var sdpLengthInBytes = getLength(sdp);
                 response = RTSP_200
                 response += "CSeq: " + headers.get("CSeq") + "\r\n";
                 response += "Content-Type: application/sdp\r\n"
                 response += "Content-Base: rtsp://localhost:8554/live.sdp/\r\n"
                 response += "Server: " + SERVER_NAME + "\r\n"
                 response += rtspDate();
-                response += "Content-Length: 449\r\n"
-                response += "\r\n"
-                response += "v=0\r\n"
-                response += "o=- 12937757946092301954 1 IN IP4 172.17.0.2\r\n"
-                response += "s=Session streamed with GStreamer\r\n"
-                response += "i=rtsp-server\r\n"
-                response += "t=0 0\r\n"
-                response += "a=tool:GStreamer\r\n"
-                response += "a=type:broadcast\r\n"
-                response += "a=control:*\r\n"
-                response += "a=range:npt=0-\r\n"
-                response += "m=video 0 RTP/AVP 96\r\n"
-                response += "c=IN IP4 0.0.0.0\r\n"
-                response += "b=AS:2097\r\n"
-                response += "a=rtpmap:96 H264/90000\r\n"
-                response += "a=framerate:25\r\n"
-                response += "a=fmtp:96 packetization-mode=1;profile-level-id=42c01f;sprop-parameter-sets=Z0LAH9oBQBbsBagICAoAAAMAAgAAAwBlHjBlQA==,aM48gA==\r\n"
-                response += "a=control:stream=0\r\n"
-                response += "a=ts-refclk:local\r\n"
-                response += "a=mediaclk:sender"
+                response += "Content-Length: " + sdpLengthInBytes + "\r\n"
+                response += sdp;
                 break;
             case "SETUP":
                 // SETUP rtsp://localhost:8554/live.sdp/stream=0 RTSP/1.0
@@ -92,9 +77,9 @@ function handleConnection(conn) {
                 var clientPorts = headers.get("Transport").split(";")[2].split("=")[1];
                 var rtpPort = clientPorts.split("-")[0];
                 var rtcpPort = clientPorts.split("-")[1];
-                var sessionId = getRandomInt(1,99999).toString();
+                var sessionId = getRandomInt(1, 99999).toString();
                 sessions.set(sessionId, rtpPort);
-                console.log("Session RTP Port: "+ sessions.get(sessionId));
+                console.log("Session RTP Port: " + sessions.get(sessionId));
                 response = RTSP_200;
                 response += "CSeq: " + headers.get("CSeq") + "\r\n";
                 response += "Transport: RTP/AVP;unicast;client_port=" + clientPorts + ";server_port=52728-52729;ssrc=40ABEB09;mode=\"PLAY\"\r\n"
@@ -116,7 +101,7 @@ function handleConnection(conn) {
                 response += "Session: " + headers.get("Session") + "\r\n"
                 response += rtspDate();
 
-                shell.exec(path.resolve(__dirname, 'rtp_serve.sh') + " " + sessions.get(headers.get("Session")) + " " + getRandomInt(0,25).toString() , {async:true, silent:true});
+                shell.exec(path.resolve(__dirname, 'rtp_serve.sh') + " " + sessions.get(headers.get("Session")) + " " + getRandomInt(0, 25).toString(), { async: true, silent: true });
                 break;
             case "TEARDOWN":
                 // TEARDOWN rtsp://localhost:8554/live.sdp/ RTSP/1.0
@@ -149,13 +134,36 @@ function handleConnection(conn) {
     }
 }
 
+function generateSdp() {
+    var sdp = "\r\n"
+    sdp += "v=0\r\n"
+    sdp += "o=- 12937757946092301954 1 IN IP4 172.17.0.2\r\n"
+    sdp += "s=Session streamed with GStreamer\r\n"
+    sdp += "i=rtsp-server\r\n"
+    sdp += "t=0 0\r\n"
+    sdp += "a=tool:GStreamer\r\n"
+    sdp += "a=type:broadcast\r\n"
+    sdp += "a=control:*\r\n"
+    sdp += "a=range:npt=0-\r\n"
+    sdp += "m=video 0 RTP/AVP 96\r\n"
+    sdp += "c=IN IP4 0.0.0.0\r\n"
+    sdp += "b=AS:2097\r\n"
+    sdp += "a=rtpmap:96 H264/90000\r\n"
+    sdp += "a=framerate:25\r\n"
+    sdp += "a=fmtp:96 packetization-mode=1;profile-level-id=42c01f;sprop-parameter-sets=Z0LAH9oBQBbsBagICAoAAAMAAgAAAwBlHjBlQA==,aM48gA==\r\n"
+    sdp += "a=control:stream=0\r\n"
+    sdp += "a=ts-refclk:local\r\n"
+    sdp += "a=mediaclk:sender"
+    return sdp;
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function rtspDate(){
+function rtspDate() {
     return "Date: " + new Date().toGMTString() + "\r\n";
 }
 
